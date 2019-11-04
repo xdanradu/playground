156d62026749ca2b6d6dc7dbf57451e94f217deb
Dan RADU <dan.radu@urss.ro>
2019-11-04 11:32:59 +0200

    event lib

--- events/src/app.js
+++ events/src/app.js
@@ -1,36 +1,12 @@
 import './styles.scss';
 import $ from '../libs/jQuery.js';
-/*import EventEmitter from 'events'
+import EventEmitter from 'events'
 var events = new EventEmitter()
 events.on('app-start', function (text) {
   console.log(text)
 })
-events.emit('app-start', 'app is running')
-*/
+
 
 $(".start-button").click(() => {
-    fillTank();
+    events.emit('app-start', 'Event generated from click handler')
 });
-
-$(".tank").on('animationend', () => {
-    if ($( ".tank" ).hasClass( "fill-tank" )) { 
-        console.log("unfill");
-        $(".tank").removeClass("fill-tank").addClass("unfill-tank");
-        $(".in").addClass("close").removeClass("open");
-        $(".out").addClass("open").removeClass("close");
-    } else {
-        $(".out").addClass("close").removeClass("open");
-    }
-});
-
-function fillTank(){
-    console.log("Tank is filling");
-    if ($( ".tank" ).hasClass( "unfill-tank" )) { 
-        console.log("Runs only once at startup");
-        $(".tank").removeClass("unfill-tank").addClass("fill-tank");
-        
-    } else {
-        $(".tank").addClass("fill-tank");
-    }
-    $(".in").addClass("open").removeClass("close");
-}
